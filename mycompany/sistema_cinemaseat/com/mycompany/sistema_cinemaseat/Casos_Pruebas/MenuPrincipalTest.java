414a6b67b9081455a1a7f7d4b8f9dfe8ad80c8ab
JohnUllaguari <149973574+JohnUllaguari@users.noreply.github.com>
2025-01-29 21:09:28 -0500

    John - MenuPrincipalTest.java

--- mycompany/sistema_cinemaseat/com/mycompany/sistema_cinemaseat/Casos_Pruebas/MenuPrincipalTest.java
+++ mycompany/sistema_cinemaseat/com/mycompany/sistema_cinemaseat/Casos_Pruebas/MenuPrincipalTest.java
@@ -1,14 +1,14 @@
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
 import java.util.stream.Stream;
+import com.mycompany.sistema_cinemaseat.*
+
 
 @ExtendWith(MockitoExtension.class) //se usa esta libreria para simular objetos y no estar instanciando
 class MenuPrincipalTest {
